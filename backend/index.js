c54d34e6593f82b35a8d57bc8dddcf7fb0f12790
Punnapareddy Bhaskar Rao <bhasaprp2020@gmail.com>
2025-07-15 23:15:03 +0530

    Add path module import in backend index.js for improved file handling

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const path=require('path')
 const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 dotenv.config();
